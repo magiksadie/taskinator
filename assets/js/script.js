c00309c7b27cb61f7107aa9b15c40608fc83f30d
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-02-23 14:43:03 -0600

    added event listener and preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 //dynamically create task item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 // create a new task on click
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
